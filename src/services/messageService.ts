cd0118b2f7ff3c7ec8d2f03ab0197f01bc0ab6e6
Sanniv <sanniv.1020@gmail.com>
2024-12-19 19:15:07 +0530

    new route

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -2,16 +2,16 @@ import { MessageData } from '../types/message';
 import { env } from '../config/environment';
 
 export async function sendMessage(data: MessageData): Promise<void> {
-  const threadEmail = env.enableCustomThreadEmail ? data.threadEmail : env.defaultThreadEmail;
+  // const threadEmail = env.enableCustomThreadEmail ? data.threadEmail : env.defaultThreadEmail;
   const apiUrl = env.apiUrl
 
-  const response = await fetch(`${apiUrl}/send-message`, {
+  const response = await fetch(`${apiUrl}/send-candidate-message`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      recipient_email: threadEmail,
+      id: "966464014",
       message_body: data.message,
     }),
   });
